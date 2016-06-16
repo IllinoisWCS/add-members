1d6b95844be8757c7331e261a778920e18010a20
Emily Chao <elchao96@live.com>
2016-06-15 23:22:45 -0700

    created the POST route

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var app = express();
+var router = express.Router();
+
+var port = process.env.port || 4000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/', router);
+
+var payloadRoute = router.route('/add-members');
+
+payloadRoute.post(function(request, response) {
+    // TODO: Consume the request's payload and extract the username
+    // TODO: Add the username to the organization if it's not already there
+    // TODO: Add the username to the team if it's not already there
+});
